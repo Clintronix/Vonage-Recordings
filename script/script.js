c546119b55718f7b81cf8a3ec5dabe1a32f266f3
Clintronix <49820287+Clintronix@users.noreply.github.com>
2020-08-19 14:52:54 -0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -6,41 +6,11 @@
 // 	x.send();
 
 function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    // if (this.readyState == 4 && this.status == 200) {
-    //   document.getElementById("demo").innerHTML = this.responseText;
-    // }
-    responseJSON = JSON.parse(this.responseText);
-    downloadRecordings(responseJSON);
-  };
-  xhttp.open("GET", "https://api.vonage.com/t/vbc.prod/call_recording/v1/api/accounts/self/users/2301047/call_recordings?call_direction=OUTBOUND&order=start%3ADESC&page=1&page_size=10", true);
-  xhttp.setRequestHeader("Accept", "application/json");
-  xhttp.setRequestHeader("Authorization", "Bearer 002c0599-b6ad-3312-987e-100f6c22f523");
-  xhttp.send();
-//console.log("xhttp: " + responseJSON);
-}
 
-
-
-function downloadRecordings(JSON) {
-  numRecordings = JSON["total_items"];
-  for (i = 0; i < numRecordings; i++) {
-    console.log(responseJSON["_embedded"]["recordings"][i]["id"]);
-  
-
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.vonage.com/t/vbc.prod/call_recording/v1/api/audio/recording/", true);
-  xhr.responseType = 'blob';
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.setRequestHeader("Authorization", "Bearer 002c0599-b6ad-3312-987e-100f6c22f523");
-  xhr.onload=function(e){download(xhr.response, "dlBinAjax.mp3", "audio/mpeg" ); }
-  xhr.send();
-  }
 var file = fs.createWriteStream("file.wav");
 var request = http.get("http://static1.grsites.com/archive/sounds/comic/comic002.wav", function(response) {
   response.pipe(file);
 });
 
 }
-
+}
